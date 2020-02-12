9fc49730f02caf210490ae0ae91078f5d1caea46
Seohyeon Kang <skang@c6r3s1.42seoul.kr>
2020-02-12 17:33:22 +0900

    c00

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,35 +6,41 @@
 /*   By: skang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 21:24:33 by skang             #+#    #+#             */
-/*   Updated: 2020/01/25 21:33:30 by skang            ###   ########.fr       */
+/*   Updated: 2020/02/12 13:36:47 by skang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_devide(int d)
+void	ft_write(int num)
 {
-	long i;
-	char c[10];
+	num = num + '0';
+	write(1, &num, 1);
+}
 
-	i = 0;
-	while (d > 0)
+void	ft_devide(int nb)
+{
+	if (nb > 9)
 	{
-		c[i] = d % 10 + '0';
-		d = d / 10;
-		i++;
+		ft_devide(nb / 10);
+		ft_write(nb % 10);
 	}
-	while (i >= 0)
-	{
-		write(1, &c[i], 1);
-		i--;
-	}
+	if (nb <= 9 && nb > 0)
+		ft_write(nb);
 }
 
 void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
+	{
 		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return ;
+	}
 	else if (nb < 0)
 	{
 		write(1, "-", 1);
